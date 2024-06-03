41ad78df7b0f0ef74e564ff2b89d118e4f233120
zoelfikars <zuulfikarfikrii@gmail.com>
2024-06-04 00:48:46 +0700

    commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Muhamad Zulfikar Fikri | 21552011172</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="bg-gray-800">
+    <div class="container mx-10 h-screen flex justify-center items-center">
+
+        <a href="#"
+            class="card block max-w-sm h-fit p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Hello World</h5>
+            <p class="font-normal text-gray-700 dark:text-gray-400">Ini adalah tugas hello world pada laravel 11.</p>
+            <p class="font-normal text-gray-700 dark:text-gray-400">Tugas ini dibuat oleh <span class="font-bold">Muhamad Zulfikar Fikri</span></p>
+        </a>
+
+
+    </div>
+</body>
+
+</html>
